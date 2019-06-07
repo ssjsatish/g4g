694feb239c00db0cb32836a56b802614d79c17f9
Satish Patel <SSJ.SATISH@GMAIL.COM>
2019-06-07 11:44:18 +0530

    kth smallest element in the array

---
+++ medium/kth_largest.py
@@ -0,0 +1,9 @@
+#Author: Satish Patel
+def kth_smallest(a,k):
+    return 0
+    
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    k = int(input())
+    print(kth_smallest(a,k))
